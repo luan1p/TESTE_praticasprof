5c6ff7e370b9ed0603eb23e42a8d811e327b4215
luan1p <cc26393@g.unicamp.br>
2026-08-06 17:08:14 -0300

    Update and rename README.md to aula1.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# TESTE_praticasprof
-Repositório para a disciplina de Práticas Profissionais 2026
---
+++ aula1.py
@@ -0,0 +1,5 @@
+# Vamos escrever um programa em python...
+
+
+msg = input('Digite seu nome: ')
+print(f'Seu nome é {msg}')
